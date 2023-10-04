54bbbae4bcc0df79eb583f4212e7ed78e9c82526
Rohit <rohit.1.kumar@stackroute.in>
2023-10-04 11:15:22 +0530

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM adoptopenjdk:11-jre-hotspot
+WORKDIR /app
+
+COPY target/demo-0.0.1-SNAPSHOT.jar demo-0.0.1-SNAPSHOT.jar
+
+EXPOSE 8080
+
+CMD ["java", "-jar", "demo-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("I'm Running");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
